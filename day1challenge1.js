73d4f8f6d080d1ffeb2d228420936f7313530164
AnnJBarr <annbarr@hotmail.com>
2019-12-03 14:02:01 +0000

    day 1 challenges completed successfully

--- day1challenge1.js
+++ day1challenge1.js
@@ -17,3 +17,31 @@ const sumFuel = array => {
     const sum = div3Round.reduce((a, b) => a + b, 0)
     return sum
 };
+
+// So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+
+// A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+// At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+// The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+
+const sumFuelforFuel = array => {
+    const fuelPerModule = array.map(n => {
+        let totFuel = 0
+        let startFuel = (Math.floor(n / 3)) - 2
+        totFuel += startFuel
+        if (startFuel > 2) {
+            do {
+                fuelforFuel = (Math.floor(startFuel / 3)) - 2;
+                startFuel = fuelforFuel
+                totFuel += fuelforFuel
+            }
+            while (startFuel > 5)
+        } else {
+            totFuel = startFuel
+        }
+        return totFuel
+    })
+    const sum = fuelPerModule.reduce((a, b) => a + b, 0)
+    return sum
+}
